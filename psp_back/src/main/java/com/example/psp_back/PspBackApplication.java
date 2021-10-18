a581151f23f687c106278e4b1430148283ba85a0
KingWJC <15110038963@163.com>
2021-10-18 17:34:18 +0800

    property service platform initial

---
+++ psp_back/src/main/java/com/example/psp_back/PspBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.psp_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PspBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PspBackApplication.class, args);
+	}
+
+}
